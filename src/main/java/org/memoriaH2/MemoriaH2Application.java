2660c303ea4c33e3b53bf1387c60b4b5a0225709
Felipe <andresfppy@gmail.com>
2024-01-12 12:09:04 -0500

    Configurar core.autocrlf en .gitignore

---
+++ src/main/java/org/memoriaH2/MemoriaH2Application.java
@@ -0,0 +1,13 @@
+package org.memoriaH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoriaH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoriaH2Application.class, args);
+	}
+
+}
